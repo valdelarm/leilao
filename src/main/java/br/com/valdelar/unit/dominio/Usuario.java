d1632ccee38bdd33440abceb3f40af3408bfb19a
vmsobrinho[gc] <vmsobrinho@stefanini.com>
2018-07-18 11:56:49 -0300

    Commit inicial

---
+++ src/main/java/br/com/valdelar/unit/dominio/Usuario.java
@@ -0,0 +1,24 @@
+package br.com.valdelar.unit.dominio;
+
+public class Usuario {
+    private int id;
+    private String nome;
+
+    public Usuario(String nome) {
+        this(0, nome);
+    }
+
+    public Usuario(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+}
